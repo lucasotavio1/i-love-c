6f36a3b5a17b19d79e30e22b9f75212bc48c4663
Lucas Otávio <lucasotavioaf@gmail.com>
2025-09-21 00:39:09 -0300

    1005

---
+++ beecrowd/1005/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+    double a, b, media;
+
+    cin >> a >> b;
+
+    media = ((a * 3.5) + (b * 7.5)) / 11.0;
+
+    cout << "MEDIA = " << fixed << setprecision(5) << media << endl;
+
+    return 0;
+}
